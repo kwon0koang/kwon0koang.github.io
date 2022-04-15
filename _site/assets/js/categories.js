1b0c092396e2961c4373e1b39ed8b3e4ac717e5d
GLORY KWON <kwon0koang@gmail.com>
2022-04-16 00:04:08 +0900

    메인 소개 수정

---
+++ _site/assets/js/categories.js
@@ -0,0 +1,29 @@
+const categories = { fiction: [{ url: `/posts/the-purpose-of-education/`, date: `12 Dec 1948`, title: `The Purpose of Education`},],jekyll: [{ url: `/posts/jekyll-markdown/`, date: `04 Nov 2021`, title: `Jekyll Markdown`},{ url: `/posts/the-purpose-of-education/`, date: `12 Dec 1948`, title: `The Purpose of Education`},], }
+
+window.onload = function () {
+  document.querySelectorAll(".category").forEach((category) => {
+    category.addEventListener("click", function (e) {
+      const posts = categories[e.target.innerText];
+      let html = ``
+      posts.forEach(post=>{
+        html += `
+        <a class="modal-article" href="${post.url}">
+          <h4>${post.title}</h4>
+          <small class="modal-article-date">${post.date}</small>
+        </a>
+        `
+      })
+      document.querySelector("#category-modal-title").innerText = e.target.innerText;
+      document.querySelector("#category-modal-content").innerHTML = html;
+      document.querySelector("#category-modal-bg").classList.toggle("open");
+      document.querySelector("#category-modal").classList.toggle("open");
+    });
+  });
+
+  document.querySelector("#category-modal-bg").addEventListener("click", function(){
+    document.querySelector("#category-modal-title").innerText = "";
+    document.querySelector("#category-modal-content").innerHTML = "";
+    document.querySelector("#category-modal-bg").classList.toggle("open");
+    document.querySelector("#category-modal").classList.toggle("open");
+  })
+};
