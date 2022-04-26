5333e48340f23a38365dc4a8ba1e57f0151252ae
GLORY KWON <kwon0koang@gmail.com>
2022-04-26 19:46:56 +0900

    [kyk] 블로그 수정

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -1,14 +1,22 @@
 ---
 ---
 
-const categories = { {% for category in site.categories %}{% capture category_name %}{{ category | first }}{% endcapture %}{{ category_name }}: [{% for post in site.categories[category_name] %}{ url: `{{ site.baseurl }}{{ post.url }}`, date: `{{post.date | date_to_string}}`, title: `{{post.title}}`},{% endfor %}],{% endfor %} }
+const categories = { 
+{% for category in site.categories %}
+  {% capture category_name %}{{ category | first }}{% endcapture %}{{ category_name }}: 
+  [
+    {% for post in site.categories[category_name] %}
+      { url: `{{ site.baseurl }}{{ post.url }}`, date: `{{post.date | date_to_string}}`, title: `{{post.title}}`},
+    {% endfor %}
+  ],
+{% endfor %} }
 
 window.onload = function () {
   document.querySelectorAll(".category").forEach((category) => {
     category.addEventListener("click", function (e) {
       const posts = categories[e.target.innerText];
       let html = ``
-      posts.forEach(post=>{
+      posts.forEach(post => {
         html += `
         <a class="modal-article" href="${post.url}">
           <h4>${post.title}</h4>
