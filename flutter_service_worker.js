47021fe1291ac6c11eb026bb35f59d1a4f1c6adf
kwon0koang <kwon0koang@gmail.com>
2023-08-28 21:50:02 +0900

    [kyk] test_flutter

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "bd0457fd6a44f420303f2e6f946d06a6",
-"index.html": "50c420960be4799e7dc7ebd6c786d085",
-"/": "50c420960be4799e7dc7ebd6c786d085",
+"index.html": "e0d0fbe08f2437bf1ee7dfafc2426a2c",
+"/": "e0d0fbe08f2437bf1ee7dfafc2426a2c",
 "styles.css": "a46b9424be3cc97b2635418f0d5cc35b",
 "loading.json": "b90bcb6ceb76c0b8d884749a00dc68e4",
 "main.dart.js": "38bb0f862ae5d11df6f041fb26512144",
